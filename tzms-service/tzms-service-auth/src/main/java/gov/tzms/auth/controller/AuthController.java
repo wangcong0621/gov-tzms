d3488b4071f309b490a172bcf71d45721278d4fe
wangcong0621 <56657191+wangcong0621@users.noreply.github.com>
2020-04-15 19:23:06 +0800

    add logger

--- tzms-service/tzms-service-auth/src/main/java/gov/tzms/auth/controller/AuthController.java
+++ tzms-service/tzms-service-auth/src/main/java/gov/tzms/auth/controller/AuthController.java
@@ -2,6 +2,8 @@ package gov.tzms.auth.controller;
 
 import gov.tzms.common.pojos.RestResult;
 import gov.tzms.common.utils.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,15 @@ import java.util.UUID;
  */
 @RestController
 public class AuthController {
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @Value("${server.port}")
     String serverPort;
 
     @GetMapping(value = "/test")
     public RestResult test() {
-        return ResultUtil.success("current server name:auth server,port:" + serverPort + "       " + UUID.randomUUID());
+        String msg = "current server name:auth server,port:" + serverPort + "       " + UUID.randomUUID();
+        logger.info(msg);
+        return ResultUtil.success(msg);
     }
 }
--- tzms-service/tzms-service-project/src/main/java/gov/tzms/project/controller/ProjectController.java
+++ tzms-service/tzms-service-project/src/main/java/gov/tzms/project/controller/ProjectController.java
@@ -3,6 +3,8 @@ package gov.tzms.project.controller;
 import gov.tzms.common.pojos.RestResult;
 import gov.tzms.common.utils.ResultUtil;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,15 @@ import java.util.UUID;
  */
 @RestController
 public class ProjectController {
+    Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
     @Value("${server.port}")
     String serverPort;
 
     @GetMapping(value = "/test")
     public RestResult test() {
-        return ResultUtil.success("current server name:project server,port:" + serverPort + "       " + UUID.randomUUID());
+        String msg = "current server name:project server,port:" + serverPort + "       " + UUID.randomUUID();
+        logger.info(msg);
+        return ResultUtil.success(msg);
     }
 }
